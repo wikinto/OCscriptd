f60c083ce0c938ad2035799cff6a4c060cd27c1d
wikinto <wikintopl@gmail.com>
2020-12-19 23:01:44 +0100

    some fix

--- geigercounter.lua
+++ geigercounter.lua
@@ -1,9 +1,10 @@
 local component = require("component")
+local computer = require("computer")
 local radiationInfo = component.nc_geiger_counter.getChunkRadiationLevel()
 while true do
     print(radiationInfo)
     os.sleep(1)
-    while radiationInfo < 3 do
+    if radiationInfo < 3 then
         computer.beep(500)
         computer.beep(2000)
     end
